9aca4b2c2edb41d9becafd03d74c9cee69831d34
rezanur <rezanur@uii.ac.id>
2018-09-11 17:36:58 +0700

    update get and get single data

--- mainApp.go
+++ mainApp.go
@@ -37,22 +37,12 @@ func initialMigration() {
 	db.AutoMigrate(&Product{})
 }
 
-func initialDatabaseConnection() {
-	db, err := gorm.Open("mysql", "root:reza@tcp(127.0.0.1:3306)/store")
-
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	defer db.Close()
-
-}
-
 // MAIN FUNCTION
 func main() {
 	initialMigration()
 	routers := mux.NewRouter()
 	routers.HandleFunc("/products", GetAllProducts).Methods("GET")
+	routers.HandleFunc("/products/{id}", GetDetailProducts).Methods("GET")
 	routers.HandleFunc("/products", CreateNewProducts).Methods("POST")
 	fmt.Println("Server serve in port 8080")
 	log.Fatal(http.ListenAndServe(":8080", routers))
@@ -60,26 +50,43 @@ func main() {
 
 // GetAllProducts function does get all products
 func GetAllProducts(w http.ResponseWriter, req *http.Request) {
-	// Intial configuration migrations
-	initialMigration()
+	db, err := gorm.Open("mysql", "root:reza@tcp(127.0.0.1:3306)/store")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
 
 	var products []Product
 	db.Find(&products)
-	fmt.Println("{}", products)
 
 	json.NewEncoder(w).Encode(products)
 }
 
+// GetDetailProducts function does get single records
+func GetDetailProducts(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("mysql", "root:reza@tcp(127.0.0.1:3306)/store")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	params := mux.Vars(r)
+	var products Product
+
+	db.First(&products, params["id"])
+	json.NewEncoder(w).Encode(&products)
+}
+
 // CreateNewProducts function does create new products
 func CreateNewProducts(w http.ResponseWriter, r *http.Request) {
-	initialDatabaseConnection()
+	db, err := gorm.Open("mysql", "root:reza@tcp(127.0.0.1:3306)/store")
 
-	vars := mux.Vars(r)
-	productCode := vars["productCode"]
-	productName := vars["productName"]
-	productPrice := vars["productPrice"]
+	if err != nil {
+		panic("failed to connect database")
+	}
 
-	db.Create(&Product{ProductCode: productCode, ProductName: productName, ProductPrice: productPrice})
-	fmt.Fprintf(w, "New user successfully created")
+	var products Product
+	json.NewDecoder(r.Body).Decode(&products)
 
+	db.Create(&products)
 }
